b56cc91e89aeb4d4209fc2ce5dc2232efefba5a4
ahmed_raza <ahmedrazanawaz8562@gmail.com>
2024-02-17 21:15:17 +0500

    dotfile updated

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-$PS1
+#!/bin/bash
+
+# Change to the directory where the script is located
+cd "$(dirname "$0")"
+
+# Create symbolic links for dotfiles
+ln -sf ~/.dotfiles/.bashrc ~/.bashrc
+# Add more symlinks for other dotfiles as needed
+
